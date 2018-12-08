0e60cf98525a996ba5d028f1a44c00c03144e8f9
Linda Chang <lindachang186@gmail.com>
2018-12-08 19:39:33 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -25,7 +25,33 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
+    html = Nokogiri::HTML(open(profile_url))
+    scraped_students= {}
 
+    html.css('div.profile-quote').each do |quote|
+      scraped_students[:profile_quote] = quote.text
+    end
+
+    html.css('div.social-icon-container').each do |info|
+      info.css('a').each do |link|
+        if link.attr('href').include?('github')
+          scraped_students[:github] = link.attr('href')
+        elsif link.attr('href').include?('linkedin')
+          scraped_students[:linkedin] = link.attr('href')
+        elsif link.attr('href').include?('twitter')
+          scraped_students[:twitter] = link.attr('href')
+        else 
+          scraped_students[:blog] = link.attr('href')
+        end
+      end
+    end
+
+    html.css('div.description-holder').each do |about|
+      scraped_students[:bio] = about.css('p').text
+    end
+
+
+    scraped_students
   end
 
 end
